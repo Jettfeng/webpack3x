bad3d563f9f2f60b4af8367534e1a2c956d90dad
lxf <1130034920@qq.com>
2017-12-23 14:40:26 +0800

    提交

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,14 @@ module.exports={
     //插件，用于生产模版和各项功能
     plugins:[],
     //配置webpack开发服务功能
-    devServer:{}
+    devServer:{
+        //设置基本目录结构
+        contentBase:path.resolve(__dirname,'dist'),
+        //服务器的IP地址，可以使用IP也可以使用localhost
+        host:'localhost',
+        //服务端压缩是否开启
+        compress:true,
+        //配置服务端口号
+        port:1018
+    }
 }
